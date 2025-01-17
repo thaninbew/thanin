42b9c0acea44648ae43a10a9c3cdc587c9a2f560
thaninbew <kongkiatsophon.t@northeastern.edu>
2025-01-17 01:28:16 -0500

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,18 +8,16 @@ import Contact from '../components/Contact';
 
 const Home: React.FC = () => {
   return (
-    <div style={{ position: 'absolute', width: '100vw', height: '100vh', overflow: 'hidden' }}>
+    <div>
       {/* Overlay */}
       <Overlay />
 
       {/* Main Content */}
-      <main style={{ position: 'absolute', top: 0, left: 0, zIndex: 900, width: '100%', height: '100%', overflow: 'hidden' }}>
         <About />
         <Experiences />
         <Projects />
         <Music />
         <Contact />
-      </main>
     </div>
   );
 };
