8a66154720dfd1cb6f9fea0614535e21366071b8
thaninbew <kongkiatsophon.t@northeastern.edu>
2025-01-23 17:13:46 -0500

    wip fixed scroll pos

--- frontend/components/About.tsx
+++ frontend/components/About.tsx
@@ -6,22 +6,23 @@ interface AboutProps {
 }
 
 const About: React.FC<AboutProps> = ({ scrollPosition }) => {
-    console.log(scrollPosition);
-    const finalPosition = 0.630194338127102; // The value to clamp at
-    const realThreshold = 0.35; // Adjust this value to control where the animation completes
-  
-    // Scale scrollPosition to align with the final position
-    const clampedScrollPosition =
-      scrollPosition <= realThreshold
-        ? (scrollPosition / realThreshold) * finalPosition
-        : finalPosition;
-  
-    const expandedStyle = {
-      width: `${44 + (100 - 44) * clampedScrollPosition}vh`, // Start at 38vh, expand to 100vh
-      height: `${30 + (40 - 30) * clampedScrollPosition}vh`, // Start at 30vh, expand to 40vh
-      transform: `translateX(${clampedScrollPosition * -215}%) translateY(${clampedScrollPosition * 50}%)`, // Move left and slightly down
-      zIndex: 10,
-    };
+  const finalPosition = 0.630194338127102; // The value to clamp at
+  const realThreshold = 0.35; // Adjust this value to control where the animation completes
+  const fixedThreshold = 0.8; // The value to switch back to relative positioning
+
+  // Scale scrollPosition to align with the final position
+  const clampedScrollPosition =
+    scrollPosition <= realThreshold
+      ? (scrollPosition / realThreshold) * finalPosition
+      : finalPosition;
+
+  const expandedStyle = {
+    width: `${44 + (100 - 44) * clampedScrollPosition}vh`, // Start at 38vh, expand to 100vh
+    height: `${30 + (40 - 30) * clampedScrollPosition}vh`, // Start at 30vh, expand to 40vh
+    transform: `translateX(${clampedScrollPosition * -215}%) translateY(${clampedScrollPosition * 50}%)`, // Move left and slightly down
+    zIndex: 10,
+    position: (scrollPosition >= realThreshold && scrollPosition < fixedThreshold ? 'fixed' : 'relative') as 'fixed' | 'relative',
+  };
 
   return (
     <div className={styles.about} style={expandedStyle}>
