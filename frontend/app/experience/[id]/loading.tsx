d1338ba4a5d0640212aa4c840810a46957de75a3
thaninbew <kongkiatsophon.t@northeastern.edu>
2025-01-28 15:10:22 -0500

    test?

--- frontend/app/experience/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../../../styles/DetailPage.module.css';
-
-export default function Loading() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.loading}>Loading experience...</div>
-    </div>
-  );
-} 
--- frontend/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app';
-import React from 'react';
-import '../styles/globals.css'; // Import global CSS
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
