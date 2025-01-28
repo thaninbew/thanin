e602af3bebd9a8b2d666d72951578d563d06c300
thaninbew <kongkiatsophon.t@northeastern.edu>
2025-01-27 22:59:00 -0500

    frame icons

--- frontend/components/Frame.tsx
+++ frontend/components/Frame.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import styles from '../styles/Frame.module.css';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Frame: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className={styles.frameContainer}>
       <div className={styles.header}>
-        <img
-          className={styles.icon}
-          src="https://c.animaapp.com/BCw7hWIC/img/---icon--github-@2x.png"
-          alt="GitHub Icon"
-        />
-        <img
-          className={styles.icon}
-          src="https://c.animaapp.com/BCw7hWIC/img/---icon--linkedin-in-@2x.png"
-          alt="LinkedIn Icon"
-        />
-        <img
-          className={styles.icon}
-          src="https://c.animaapp.com/BCw7hWIC/img/---icon--envelope-closed-@2x.png"
-          alt="Mail Icon"
-        />
+        <a 
+          href="https://github.com/thaninbew" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className={styles.iconLink}
+        >
+          <FaGithub size={24} className={styles.icon} />
+        </a>
+        <a 
+          href="https://linkedin.com/in/thaninbew" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className={styles.iconLink}
+        >
+          <FaLinkedin size={24} className={styles.icon} />
+        </a>
+        <a 
+          href="mailto:bewxtt@gmail.com"
+          className={styles.iconLink}
+        >
+          <FaEnvelope size={24} className={styles.icon} />
+        </a>
       </div>
       {children}
       <div className={styles.footer}></div>
