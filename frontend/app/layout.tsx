847945284202f92b5d069c39c2c0f8245f121623
thaninbew <kongkiatsophon.t@northeastern.edu>
2025-02-01 15:10:13 -0500

    name and image meta

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,22 +2,22 @@ import type { Metadata } from 'next';
 import '../styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'Thanin Kongkiatsophon',
+  title: 'Thanin K. Portfolio',
   description: 'Personal portfolio website of Thanin (Bew) Kongkiatsophon showcasing projects and experiences',
   metadataBase: new URL('https://thanin.dev'),
   icons: {
-    icon: 'https://res.cloudinary.com/dez4qkb8z/image/upload/v1738383815/portfolio/projects/images/f2yubzhehwn4bqrtfedr.png',
+    icon: 'https://res.cloudinary.com/dez4qkb8z/image/upload/v1738440450/portfolio/projects/images/uq5znpvme1w3gbzwgxlr.png',
   },
   openGraph: {
-    title: 'Thanin Kongkiatsophon',
+    title: 'Thanin K. Portfolio',
     description: 'Personal portfolio website of Thanin (Bew) Kongkiatsophon showcasing projects and experiences',
-    images: ['https://res.cloudinary.com/dez4qkb8z/image/upload/v1738383815/portfolio/projects/images/f2yubzhehwn4bqrtfedr.png'],
+    images: ['https://res.cloudinary.com/dez4qkb8z/image/upload/v1738440450/portfolio/projects/images/uq5znpvme1w3gbzwgxlr.png'],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Thanin Kongkiatsophon',
+    title: 'Thanin K. Portfolio',
     description: 'Personal portfolio website of Thanin (Bew) Kongkiatsophon showcasing projects and experiences',
-    images: ['https://res.cloudinary.com/dez4qkb8z/image/upload/v1738383815/portfolio/projects/images/f2yubzhehwn4bqrtfedr.png'],
+    images: ['https://res.cloudinary.com/dez4qkb8z/image/upload/v1738440450/portfolio/projects/images/uq5znpvme1w3gbzwgxlr.png'],
   },
 };
 
